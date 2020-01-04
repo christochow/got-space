8cefec4fae038a4aafd45110a7f6405d82f8cc93
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2020-01-03 22:01:57 -0500

    added list school name function

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.getSchools = functions.https.onRequest(async (request, response) => {
+ const data = await db.collection('schools').get();
+ response.send(JSON.stringify(data.docs.map(e=>e.id)));
+});
+
