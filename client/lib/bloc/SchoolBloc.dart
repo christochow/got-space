51a949652857ce27ac7bcfc31b819ee1777af763
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2020-01-09 17:18:54 -0500

    added call to get Data

--- client/lib/bloc/SchoolBloc.dart
+++ client/lib/bloc/SchoolBloc.dart
@@ -1,19 +1,42 @@
-import 'package:bloc/bloc.dart';
-import 'package:got_space/Repositories/FirebaseRepository.dart';
+import 'dart:async';
 
-class SchoolBloc extends Bloc<String, String>{
+import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:got_space/Repositories/FirebaseRepository.dart';
+import 'package:got_space/models/BlocEvent.dart';
+import 'package:got_space/models/BlocEventType.dart';
+import 'package:got_space/models/BlocState.dart';
+
+class SchoolBloc extends Bloc<BlocEvent, BlocState>{
 
   FirebaseRepository _firebaseRepository;
+  StreamSubscription _subscription;
+  String _id;
 
-  SchoolBloc(FirebaseRepository repo){
+  SchoolBloc(FirebaseRepository repo, String id){
     _firebaseRepository = repo;
+    _id = id;
+    _subscription = _firebaseRepository.getDataFromPath(_id).listen((snapshot){
+      this.add(BlocEvent(BlocEventType.ADD, snapshot));
+    });
+  }
+
+  StreamSubscription get subscription => _subscription;
+
+  @override
+  BlocState get initialState => BlocState(null);
+
+  @override
+  Stream<BlocState> mapEventToState(BlocEvent event) async*{
+    if(event.type==BlocEventType.ADD){
+      yield BlocState(event.snapshot);
+    }
   }
 
   @override
-  String get initialState => '';
-
-  @override
-  Stream<String> mapEventToState(String event) async*{
-    yield 'null';
+  Future<void> close() {
+    _subscription?.cancel();
+    return super.close();
   }
 }
+
