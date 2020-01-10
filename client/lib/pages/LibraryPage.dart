1d93112357bc224b3ccaae8d635fb0f0cdf57553
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2020-01-10 16:38:18 -0500

    create library page and link with library list

---
+++ client/lib/pages/LibraryPage.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:got_space/Repositories/FirebaseRepository.dart';
+import 'package:got_space/bloc/LibraryBloc.dart';
+import 'package:got_space/bloc/SchoolBloc.dart';
+import 'package:got_space/bloc/SubSectionBloc.dart';
+import 'package:got_space/client/FirebaseClient.dart';
+import 'package:got_space/models/BlocState.dart';
+
+class LibraryPage extends StatefulWidget {
+  LibraryPage({Key key, this.libBloc, this.path}) : super(key: key);
+  final LibraryBloc libBloc;
+  final String path;
+
+  @override
+  _LibraryPageState createState() => _LibraryPageState();
+}
+
+class _LibraryPageState extends State<LibraryPage> {
+  
+  Stream<BlocState> _stream;
+  
+  @override
+  void initState() {
+    _stream = widget.libBloc.asBroadcastStream();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.libBloc.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _stream,
+      builder: (BuildContext context, AsyncSnapshot<BlocState> state) {
+        if(state.data == null){
+          return Center(child: Text('Loading'),);
+        }
+        return Scaffold(
+            appBar: AppBar(
+              title: Text(state.data.snapshot.documentID),
+            ),
+            body: Center(
+              child: Text(
+                  'Rating: ' + state.data.snapshot.data['rating'].toString()),
+            ));
+//      return ListView(
+//        children: state.subSections.map((e) {
+//          return BlocProvider<SubSectionBloc>(
+//            create: (context) =>
+//                SubSectionBloc(FirebaseRepository(FirebaseClient()), e.documentID,
+//                    'widget.path' + '/floors/'),
+//            child: FlatButton(child: Text(e.documentID+':'+e.data['rating']),
+//              onPressed: () {
+//                Navigator.push(context, MaterialPageRoute(builder: (
+//                    context) => FloorList()));
+//              },),
+//          );
+//        }).toList(),
+//      );
+      },
+    );
+  }
+}
--- client/lib/pages/tabs/LibraryList.dart
+++ client/lib/pages/tabs/LibraryList.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:got_space/Repositories/FirebaseRepository.dart';
+import 'package:got_space/bloc/LibraryBloc.dart';
 import 'package:got_space/bloc/SchoolBloc.dart';
+import 'package:got_space/client/FirebaseClient.dart';
 import 'package:got_space/models/BlocState.dart';
+import 'package:got_space/pages/LibraryPage.dart';
 
 class LibraryList extends StatefulWidget {
-  LibraryList({Key key}) : super(key: key);
+  LibraryList({Key key, this.path}) : super(key: key);
+  final String path;
 
   @override
   _LibraryListState createState() => _LibraryListState();
 }
 
 class _LibraryListState extends State<LibraryList> {
+
 
   @override
   void initState() {
@@ -20,11 +26,31 @@ class _LibraryListState extends State<LibraryList> {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SchoolBloc, BlocState>(
-      builder: (context, state){
-        if(state.snapshot==null){
+      builder: (context, state) {
+        if (state.snapshot == null) {
           return Text('Loading');
         }
-        return Text(state.snapshot.data['rating'].toString());
+        return ListView(
+          shrinkWrap: true,
+          children: state.subSections.map((e) {
+            LibraryBloc _bloc = LibraryBloc(FirebaseRepository(FirebaseClient()),
+                e.documentID, widget.path);
+            return BlocProvider<LibraryBloc>(
+              create: (context) => _bloc,
+              child: FlatButton(
+                child: Text(e.documentID + ':' + e.data['rating'].toString()),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => LibraryPage(
+                              libBloc: _bloc,
+                              path: widget.path + '/' + e.documentID)));
+                },
+              ),
+            );
+          }).toList(),
+        );
       },
     );
   }
