da9eed7a59daffe3df20c184214a759a784afed1
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2020-01-12 01:15:04 -0500

    removed redundant subscription

--- client/lib/bloc/SubSectionBloc.dart
+++ client/lib/bloc/SubSectionBloc.dart
@@ -9,24 +9,12 @@ import 'package:got_space/models/BlocState.dart';
 class SubSectionBloc extends Bloc<BlocEvent, BlocState> {
   FirebaseRepository _firebaseRepository;
   StreamSubscription _subscription;
-  StreamSubscription _subscription2;
 
   SubSectionBloc(FirebaseRepository repo, String id, String path) {
     _firebaseRepository = repo;
     _subscription =
         _firebaseRepository.getDataFromPath(path, id).listen((snapshot) {
       this.add(BlocEvent(BlocEventType.ADD, snapshot, null));
-      if(snapshot.data['hasChildren']==true){
-        _subscribetoSub(path, id);
-      }
-    });
-  }
-
-  void _subscribetoSub(String path, String id){
-    _subscription2 = _firebaseRepository
-        .getCollectionFromPath(path, id + '/subsections')
-        .listen((snapshot) {
-      this.add(BlocEvent(BlocEventType.SUB, null, snapshot.documents));
     });
   }
 
@@ -39,15 +27,12 @@ class SubSectionBloc extends Bloc<BlocEvent, BlocState> {
   Stream<BlocState> mapEventToState(BlocEvent event) async* {
     if (event.type == BlocEventType.ADD) {
       yield BlocState(event.snapshot, state.subSections);
-    } else if (event.type == BlocEventType.SUB) {
-      yield BlocState(state.snapshot, event.subSections);
     }
   }
 
   @override
   Future<void> close() {
     _subscription?.cancel();
-    _subscription2?.cancel();
     return super.close();
   }
 }
