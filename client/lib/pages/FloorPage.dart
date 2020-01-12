5b9833d0565e66407737bed0a7814b4abe03efed
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2020-01-12 01:13:48 -0500

    created SubSectionPage and added navigation to page

--- client/lib/pages/FloorPage.dart
+++ client/lib/pages/FloorPage.dart
@@ -3,15 +3,17 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:got_space/Repositories/FirebaseRepository.dart';
+import 'package:got_space/bloc/FloorBloc.dart';
 import 'package:got_space/bloc/LibraryBloc.dart';
 import 'package:got_space/bloc/SchoolBloc.dart';
 import 'package:got_space/bloc/SubSectionBloc.dart';
 import 'package:got_space/client/FirebaseClient.dart';
 import 'package:got_space/models/BlocState.dart';
+import 'package:got_space/pages/SubSectionPage.dart';
 
 class FloorPage extends StatefulWidget {
-  FloorPage({Key key, this.subSecBloc, this.path}) : super(key: key);
-  final SubSectionBloc subSecBloc;
+  FloorPage({Key key, this.floorBloc, this.path}) : super(key: key);
+  final FloorBloc floorBloc;
   final String path;
 
   @override
@@ -23,7 +25,7 @@ class _FloorPageState extends State<FloorPage> {
 
   @override
   void initState() {
-    _stream = widget.subSecBloc.asBroadcastStream();
+    _stream = widget.floorBloc.asBroadcastStream();
     super.initState();
   }
 
@@ -53,18 +55,20 @@ class _FloorPageState extends State<FloorPage> {
                       state.data.snapshot.data['rating'].toString())
                 ],
                 state.data.subSections.map((e) {
+                  SubSectionBloc _bloc = SubSectionBloc(
+                      FirebaseRepository(FirebaseClient()),
+                      e.documentID,
+                      widget.path);
                   return BlocProvider<SubSectionBloc>(
-                    create: (context) =>
-                        SubSectionBloc(
-                            FirebaseRepository(FirebaseClient()),
-                            e.documentID,
-                            'widget.path' + '/floors/'),
+                    create: (context) => _bloc,
                     child: FlatButton(
                       child: Text(e.documentID + ':' + e.data['rating']),
                       onPressed: () {
-//                        Navigator.push(context,
-//                            MaterialPageRoute(
-//                                builder: (context) => ()));
+                        Navigator.push(context,
+                            MaterialPageRoute(
+                                builder: (context) => SubSectionPage(
+                                  subSecBloc: _bloc,
+                                )));
                       },
                     ),
                   );
---
+++ client/lib/pages/SubSectionPage.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:got_space/Repositories/FirebaseRepository.dart';
+import 'package:got_space/bloc/FloorBloc.dart';
+import 'package:got_space/bloc/LibraryBloc.dart';
+import 'package:got_space/bloc/SchoolBloc.dart';
+import 'package:got_space/bloc/SubSectionBloc.dart';
+import 'package:got_space/client/FirebaseClient.dart';
+import 'package:got_space/models/BlocState.dart';
+
+class SubSectionPage extends StatefulWidget {
+  SubSectionPage({Key key, this.subSecBloc}) : super(key: key);
+  final SubSectionBloc subSecBloc;
+
+  @override
+  _SubSectionPageState createState() => _SubSectionPageState();
+}
+
+class _SubSectionPageState extends State<SubSectionPage> {
+  Stream<BlocState> _stream;
+
+  @override
+  void initState() {
+    _stream = widget.subSecBloc.asBroadcastStream();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _stream,
+      builder: (BuildContext context, AsyncSnapshot<BlocState> state) {
+        if (state.data == null) {
+          return Center(
+            child: Text('Loading'),
+          );
+        }
+        return Scaffold(
+            appBar: AppBar(
+              title: Text(state.data.snapshot.documentID),
+            ),
+            body: ListView(
+              children: [
+                Text('Rating: ' + state.data.snapshot.data['rating'].toString())
+              ],
+            ));
+      },
+    );
+  }
+}
