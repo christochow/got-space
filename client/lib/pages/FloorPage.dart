090dfe1dfd3ac417461f18eb18986238901fe248
Chow Lok Man Christopher <christochow@yahoo.com.hk>
2020-01-14 00:34:53 -0500

    added alert dialog to submit rating

--- client/lib/pages/FloorPage.dart
+++ client/lib/pages/FloorPage.dart
@@ -1,12 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:got_space/Repositories/FirebaseRepository.dart';
 import 'package:got_space/bloc/FloorBloc.dart';
+import 'package:got_space/bloc/InputBloc.dart';
 import 'package:got_space/bloc/LibraryBloc.dart';
 import 'package:got_space/bloc/SubSectionBloc.dart';
 import 'package:got_space/client/FirebaseClient.dart';
 import 'package:got_space/models/BlocState.dart';
+import 'package:got_space/models/InputEvent.dart';
 import 'package:got_space/pages/SubSectionPage.dart';
 
 class FloorPage extends StatefulWidget {
@@ -22,6 +25,9 @@ class FloorPage extends StatefulWidget {
 }
 
 class _FloorPageState extends State<FloorPage> {
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -33,7 +39,81 @@ class _FloorPageState extends State<FloorPage> {
     super.dispose();
   }
 
-  _showInputDialog() {}
+  _displaySnackBar(BuildContext context, String msg) {
+    final snackBar = SnackBar(content: Text(msg));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  _showInputDialog(String path, BuildContext rootContext) {
+    return showDialog(
+        context: context,
+        builder: (parentCtx) {
+          return BlocProvider<InputBloc>(
+              create: (context) =>
+                  InputBloc(FirebaseRepository(FirebaseClient())),
+              child: BlocBuilder<InputBloc, String>(
+                builder: (BuildContext context, String state) {
+                  InputBloc _bloc = BlocProvider.of<InputBloc>(context);
+                  return AlertDialog(
+                    title: Text('Submit a rating'),
+                    content: Form(
+                      key: _formKey,
+                      child: TextFormField(
+                          initialValue: '1',
+                          validator: (value) {
+                            if(value.isEmpty){
+                              return 'Field must not be empty!';
+                            }
+                            var n = num.parse(value);
+                            if(n<1 || n>10){
+                              return 'Input must be between 1 to 10!';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _bloc.add(InputEvent(path, num.parse(value)));
+                            Navigator.of(parentCtx).pop();
+                            _bloc.asBroadcastStream().listen((String state) {
+                              if(state!=''){
+                                _bloc.close();
+                                _formKey.currentState?.reset();
+                                _displaySnackBar(rootContext, state);
+                              }
+                            });
+                          },
+                          keyboardType: TextInputType.numberWithOptions(
+                              signed: false, decimal: false),
+                          decoration: InputDecoration(),
+                          inputFormatters: [
+                            WhitelistingTextInputFormatter.digitsOnly
+                          ]),
+                    ),
+                    actions: <Widget>[
+                      FlatButton(
+                          child: new Text('Cancel'),
+                          onPressed: () {
+                            _bloc.close();
+                            _formKey.currentState.reset();
+                            Navigator.of(parentCtx).pop();
+                          }),
+                      FlatButton(
+                        child: new Text('Submit'),
+                        onPressed: () {
+                          List<String> pathList = path.split('/');
+                          pathList[0] = 'records';
+                          pathList.add('records');
+                          path = pathList.join('/');
+                          if (_formKey.currentState.validate()) {
+                            _formKey.currentState.save();
+                          }
+                        },
+                      )
+                    ],
+                  );
+                },
+              ));
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,48 +134,52 @@ class _FloorPageState extends State<FloorPage> {
               appBar: AppBar(
                 title: Text(snapshot.documentID),
               ),
-              body: ListView(
-                children: [
-                  [
-                    Center(
-                      child: Text('Rating: ' +
-                          num.parse(snapshot.data['rating'].toString())
-                              .toStringAsFixed(1)),
-                    ),
-                    Visibility(
-                      visible: !snapshot.data['hasChild'],
-                      child: FlatButton(
-                        child: Text('Submit a rating'),
-                        onPressed: _showInputDialog,
-                      ),
-                    )
-                  ],
-                  state.subSections.map((e) {
-                    SubSectionBloc _bloc = SubSectionBloc(
-                        FirebaseRepository(FirebaseClient()),
-                        e.documentID,
-                        widget.path);
-                    return BlocProvider<SubSectionBloc>(
-                      create: (context) => _bloc,
-                      child: FlatButton(
-                        child: Text(e.documentID +
-                            ':' +
-                            num.parse(e.data['rating'].toString())
+              body: Builder(
+                builder: (context) => ListView(
+                  children: [
+                    [
+                      Center(
+                        child: Text('Rating: ' +
+                            num.parse(snapshot.data['rating'].toString())
                                 .toStringAsFixed(1)),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SubSectionPage(
-                                        subSecBloc: _bloc,
-                                        floorBloc: widget.floorBloc,
-                                        id: e.documentID,
-                                      )));
-                        },
                       ),
-                    );
-                  }).toList()
-                ].expand((e) => e).toList(),
+                      Visibility(
+                        visible: !snapshot.data['hasChild'],
+                        child: FlatButton(
+                          child: Text('Submit a rating'),
+                          onPressed: () {
+                            _showInputDialog(snapshot.reference.path, context);
+                          },
+                        ),
+                      )
+                    ],
+                    state.subSections.map((e) {
+                      SubSectionBloc _bloc = SubSectionBloc(
+                          FirebaseRepository(FirebaseClient()),
+                          e.documentID,
+                          widget.path);
+                      return BlocProvider<SubSectionBloc>(
+                        create: (context) => _bloc,
+                        child: FlatButton(
+                          child: Text(e.documentID +
+                              ':' +
+                              num.parse(e.data['rating'].toString())
+                                  .toStringAsFixed(1)),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SubSectionPage(
+                                          subSecBloc: _bloc,
+                                          floorBloc: widget.floorBloc,
+                                          id: e.documentID,
+                                        )));
+                          },
+                        ),
+                      );
+                    }).toList()
+                  ].expand((e) => e).toList(),
+                ),
               ));
         },
       ),
--- client/lib/pages/SubSectionPage.dart
+++ client/lib/pages/SubSectionPage.dart
@@ -1,9 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:got_space/Repositories/FirebaseRepository.dart';
 import 'package:got_space/bloc/FloorBloc.dart';
+import 'package:got_space/bloc/InputBloc.dart';
 import 'package:got_space/bloc/SubSectionBloc.dart';
+import 'package:got_space/client/FirebaseClient.dart';
 import 'package:got_space/models/BlocState.dart';
+import 'package:got_space/models/InputEvent.dart';
 
 class SubSectionPage extends StatefulWidget {
   SubSectionPage({Key key, this.subSecBloc, this.floorBloc, this.id})
@@ -17,6 +22,9 @@ class SubSectionPage extends StatefulWidget {
 }
 
 class _SubSectionPageState extends State<SubSectionPage> {
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -28,7 +36,81 @@ class _SubSectionPageState extends State<SubSectionPage> {
     super.dispose();
   }
 
-  _showInputDialog() {}
+  _displaySnackBar(BuildContext context, String msg) {
+    final snackBar = SnackBar(content: Text(msg));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  _showInputDialog(String path, BuildContext rootContext) {
+    return showDialog(
+        context: context,
+        builder: (parentCtx) {
+          return BlocProvider<InputBloc>(
+              create: (context) =>
+                  InputBloc(FirebaseRepository(FirebaseClient())),
+              child: BlocBuilder<InputBloc, String>(
+                builder: (BuildContext context, String state) {
+                  InputBloc _bloc = BlocProvider.of<InputBloc>(context);
+                  return AlertDialog(
+                    title: Text('Submit a rating'),
+                    content: Form(
+                      key: _formKey,
+                      child: TextFormField(
+                          initialValue: '1',
+                          validator: (value) {
+                            if(value.isEmpty){
+                              return 'Field must not be empty!';
+                            }
+                            var n = num.parse(value);
+                            if(n<1 || n>10){
+                              return 'Input must be between 1 to 10!';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _bloc.add(InputEvent(path, num.parse(value)));
+                            Navigator.of(parentCtx).pop();
+                            _bloc.asBroadcastStream().listen((String state) {
+                              if(state!=''){
+                                _bloc.close();
+                                _formKey.currentState?.reset();
+                                _displaySnackBar(rootContext, state);
+                              }
+                            });
+                          },
+                          keyboardType: TextInputType.numberWithOptions(
+                              signed: false, decimal: false),
+                          decoration: InputDecoration(),
+                          inputFormatters: [
+                            WhitelistingTextInputFormatter.digitsOnly
+                          ]),
+                    ),
+                    actions: <Widget>[
+                      FlatButton(
+                          child: new Text('Cancel'),
+                          onPressed: () {
+                            _bloc.close();
+                            _formKey.currentState.reset();
+                            Navigator.of(parentCtx).pop();
+                          }),
+                      FlatButton(
+                        child: new Text('Submit'),
+                        onPressed: () {
+                          List<String> pathList = path.split('/');
+                          pathList[0] = 'records';
+                          pathList.add('records');
+                          path = pathList.join('/');
+                          if (_formKey.currentState.validate()) {
+                            _formKey.currentState.save();
+                          }
+                        },
+                      )
+                    ],
+                  );
+                },
+              ));
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,19 +128,21 @@ class _SubSectionPageState extends State<SubSectionPage> {
             appBar: AppBar(
               title: Text(snapshot.documentID),
             ),
-            body: ListView(
-              children: <Widget>[
-                Center(
-                  child: Text('Rating: ' +
-                      num.parse(snapshot.data['rating'].toString())
-                          .toStringAsFixed(1)),
-                ),
-                FlatButton(
-                  child: Text('Submit a rating'),
-                  onPressed: _showInputDialog,
-                )
-              ],
-            ));
+            body: Builder(
+                builder: (context) => ListView(
+                      children: <Widget>[
+                        Center(
+                          child: Text('Rating: ' +
+                              num.parse(snapshot.data['rating'].toString())
+                                  .toStringAsFixed(1)),
+                        ),
+                        FlatButton(
+                          child: Text('Submit a rating'),
+                          onPressed: () => _showInputDialog(
+                              snapshot.reference.path, context),
+                        )
+                      ],
+                    )));
       },
     );
   }
